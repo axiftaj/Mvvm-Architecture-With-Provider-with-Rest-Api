f31eea99a5f6c80c4bff6b34e2f8b17f2a280e4c
Asif Taj <axiftaj@gmail.com>
2024-01-30 15:48:58 +0500

    dead code removed

--- lib/configs/utils.dart
+++ lib/configs/utils.dart
@@ -6,27 +6,16 @@ import 'package:fluttertoast/fluttertoast.dart';
 class Utils {
 
 
-  static double averageRating(List<int> rating){
-    var avgRating = 0;
-    for(int i = 0 ; i< rating.length ; i++){
-      avgRating = avgRating + rating[i];
-    }
-    return double.parse((avgRating/rating.length).toStringAsFixed(1)) ;
-  }
-
   static void fieldFocusChange(BuildContext context , FocusNode current , FocusNode nextFocus){
     current.unfocus();
     FocusScope.of(context).requestFocus(nextFocus);
   }
-
-
 
   static toastMessage(String message){
     Fluttertoast.showToast(
         msg: message,
     backgroundColor: Colors.black,
       textColor: Colors.white,
-
     );
   }
 
